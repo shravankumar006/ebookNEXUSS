9af5eb8e0dbda88b294d9434692ead1d04c5ecc0
Shravan <shravan1458119@gmail.com>
2024-07-30 07:52:27 +0530

    Initial commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -8,24 +9,36 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-const URI = process.env.MongoDBURI;
+ const URI = process.env.MONGODBANSH;
 
-// connect to mongoDB
+// // connect to mongoDB
+// try {
+//     mongoose.connect(URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     });
+//     console.log("Connected to mongoDB");
+// } catch (error) {
+//     console.log("Error: ", error);
+//}
+
+
+
+// Or:
 try {
-    mongoose.connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log("Connected to mongoDB");
+  await mongoose.connect('mongodb+srv://shravan1458119:14581119ansh@ansh.c5gxcok.mongodb.net/?retryWrites=true&w=majority&appName=ANSH');
+  console.log("MOngoDB connected");
 } catch (error) {
-    console.log("Error: ", error);
+  handleError(error);
 }
+
+//run().catch(console.dir);
 
 // defining routes
 app.use("/book", bookRoute);
